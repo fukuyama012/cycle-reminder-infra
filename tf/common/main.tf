3a85164109c8a43ae2a79ffc0634c92643bc2e07
Shingo Fukuyama <fukuyama012@gmail.com>
2020-01-04 23:23:34 +0900

    setup gcs, terraform/state

---
+++ tf/common/main.tf
@@ -0,0 +1,7 @@
+output "region" {
+  value = "us-central1"
+}
+
+output "zone" {
+  value = "us-central1-b"
+}
---
+++ tf/state/main.tf
@@ -0,0 +1,41 @@
+variable "project" {}
+
+locals {
+  bucket_name = "cr-terraform-state"
+}
+
+provider "google" {
+  project = var.project
+  region  = module.common.region
+  zone    = module.common.zone
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "cr-terraform-state"
+    prefix = "terraform/state"
+  }
+}
+
+module "common" {
+  source = "../common"
+}
+
+resource "google_storage_bucket" "terraform-state-store" {
+  name     = local.bucket_name
+  location = module.common.region
+  storage_class = "REGIONAL"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      num_newer_versions = 5
+    }
+  }
+}
